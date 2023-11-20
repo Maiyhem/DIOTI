047755074d32f2a63d15e6a928636210b7b12a2c
davoodkhobbakht <davoodkhobbakht@gmail.com>
2023-11-20 14:20:26 +0330

    add aed price

--- price_scrapper/models.py
+++ price_scrapper/models.py
@@ -4,6 +4,7 @@ class Product(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     web_price = models.CharField(max_length =  20 ,null=True, blank=True)
+    AED_price = models.CharField(max_length =  20 ,null=True, blank=True)
     scrap_price = models.CharField(max_length =  20 ,null=True, blank=True)
     target_link = models.URLField(max_length=255, null=True, blank=True)
     crawl_status = models.CharField(max_length=50, default="No Price")
